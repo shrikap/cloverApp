beaadd3f93e686495b6c050f00081c4c9e53305e
shrikap <shrikapunuganti@gmail.com>
2023-07-18 11:07:41 -0700

    Initial Commit

---
+++ cloverApp/cloverAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloverAppApp.swift
+//  cloverApp
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct cloverAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
